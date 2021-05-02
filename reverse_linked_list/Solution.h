bb5a2f136cbfcc0831f2bfada28a1a5d17cb0581
Andrew Burger <andrewburger1991@gmail.com>
2021-05-02 00:05:15 -0700

    reverse linked_list iteratively

---
+++ reverse_linked_list/Solution.h
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+        ListNode *current = head;
+        ListNode *temp = nullptr;
+        ListNode *p_previous = nullptr;
+
+        while (current) {
+
+            temp = current->next;
+            current->next = p_previous;
+            p_previous = current;
+            current = temp;
+
+            if (!current) {
+                head = p_previous;
+            }
+        }
+
+        return head;
+    }
+};
